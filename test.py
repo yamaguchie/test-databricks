e3a6b92fedfb36a4d9ab10bec264e6a4c6e72cc0
yamaguchie <yamaguchie>
2023-12-17 05:21:47 +0000

    aaa

---
+++ test.py
@@ -0,0 +1,19 @@
+import pyspark.pandas as ps
+import pandas as pd
+
+data = {'name': ['Alice', 'Bob', 'Charlie', 'David', 'Ella'],
+        'age': [25, 30, 18, 42, 33],
+        'country': ['USA', 'Canada', 'UK', 'USA', 'Australia']}
+df = pd.DataFrame(data)
+sdf = ps.from_pandas(df)
+
+ps.sql("use catalog catalog_kadokura")
+ps.sql("use database default")
+# sdf.to_table("catalog_kadokura.default.zzz",format="delta",mode="append")
+sdf.to_table("jjj",format="delta",mode="append")
+
+tmp = ps.read_table("jjj")
+
+tmp.to_table("z200")
+
+
