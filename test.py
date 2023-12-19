0b6dcea36c23a96f4ec8017f4839756a763263ae
yamaguchie <yamaguchie>
2023-12-19 01:20:54 +0000

    hoge

--- test.py
+++ test.py
@@ -1,19 +1,21 @@
 import pyspark.pandas as ps
 import pandas as pd
 
-data = {'name': ['Alice', 'Bob', 'Charlie', 'David', 'Ella'],
-        'age': [25, 30, 18, 42, 33],
-        'country': ['USA', 'Canada', 'UK', 'USA', 'Australia']}
-df = pd.DataFrame(data)
-sdf = ps.from_pandas(df)
 
-ps.sql("use catalog catalog_kadokura")
-ps.sql("use database default")
-# sdf.to_table("catalog_kadokura.default.zzz",format="delta",mode="append")
-sdf.to_table("jjj",format="delta",mode="append")
+def hoge():
+    data = {
+        "name": ["Alice", "Bob", "Charlie", "David", "Ella"],
+        "age": [25, 30, 18, 42, 33],
+        "country": ["USA", "Canada", "UK", "USA", "Australia"],
+    }
+    df = pd.DataFrame(data)
+    sdf = ps.from_pandas(df)
 
-tmp = ps.read_table("jjj")
+    ps.sql("use catalog catalog_kadokura")
+    ps.sql("use database default")
 
-tmp.to_table("z300")
+    sdf.to_table("catalog_kadokura.default.zzz", format="delta", mode="append")
 
+    tmp = ps.read_table("catalog_kadokura.default.zzz")
 
+    tmp.to_table("z300")
